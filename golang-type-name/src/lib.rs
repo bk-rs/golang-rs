190c443fba91fff497fa4c99009d3d7d1b40c78f
vkill <vkill.net@gmail.com>
2021-04-11 22:53:59 +0800

    Add basic types to golang-type-name crate

--- golang-type-name/src/lib.rs
+++ golang-type-name/src/lib.rs
@@ -1,1 +1,35 @@
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum TypeName {
+    // https://golang.org/ref/spec#Boolean_types
+    // https://github.com/golang/go/blob/go1.16.3/src/builtin/builtin.go#L13-L14
+    Bool,
+    // https://golang.org/ref/spec#Numeric_types
+    // https://github.com/golang/go/blob/go1.16.3/src/builtin/builtin.go#L22-L66
+    // https://github.com/golang/go/blob/go1.16.3/src/builtin/builtin.go#L73-L92
+    Uint8,
+    Uint16,
+    Uint32,
+    Uint64,
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    Float32,
+    Float64,
+    Complex64,
+    Complex128,
+    Uint,
+    Int,
+    Uintptr,
+    // https://golang.org/ref/spec#String_types
+    // https://github.com/golang/go/blob/go1.16.3/src/builtin/builtin.go#L68-L71
+    String,
+    // https://golang.org/ref/spec#QualifiedIdent
+    QualifiedIdent(PackageName, String),
+    //
+    Identifier(String),
+}
+pub type PackageName = String;
 
+#[derive(thiserror::Error, Debug)]
+pub enum TypeNameParseError {}
