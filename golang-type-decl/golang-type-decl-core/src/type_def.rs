0382af18ecd536780d7c6fa8698b64fdf8e33aae
vkill <vkill.net@gmail.com>
2021-04-19 11:24:16 +0800

    Refactor golang-type-decl

--- golang-type-decl/golang-type-decl-core/src/type_def.rs
+++ golang-type-decl/golang-type-decl-core/src/type_def.rs
@@ -1,7 +1,7 @@
 use std::str;
 
 use golang_parser::tree_sitter::Node;
-use golang_type_core::{Type, TypeParseError};
+use golang_type_core::{StructType, Type, TypeParseError};
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct TypeDef {
@@ -40,5 +40,67 @@ impl TypeDef {
             name: name.to_owned(),
             r#type,
         })
+    }
+}
+
+pub struct JsonStructDef {
+    pub name: String,
+    pub struct_type: StructType,
+}
+
+#[cfg(feature = "enable-quote-to_tokens")]
+pub mod enable_quote_to_tokens {
+    use super::JsonStructDef;
+
+    use convert_case::{Case, Casing as _};
+    use golang_type_core::StructField;
+    use proc_macro2::TokenStream;
+    use quote::{format_ident, quote, ToTokens, TokenStreamExt as _};
+
+    impl ToTokens for JsonStructDef {
+        fn to_tokens(&self, tokens: &mut TokenStream) {
+            let struct_name = format_ident!("{}", self.name.to_case(Case::Snake));
+            let struct_fields: Vec<_> = self
+                .struct_type
+                .field_decls
+                .iter()
+                .map(|field_decl| match &field_decl.struct_field {
+                    StructField::IdentifierListType(names, r#type) => names
+                        .iter()
+                        .map(|name| {
+                            let field_name_serde_rename = name;
+                            let field_name = format_ident!("{}", name.to_case(Case::Snake));
+                            let field_type = r#type;
+
+                            quote! {
+                                #[serde(rename = #field_name_serde_rename)]
+                                pub #field_name: #field_type,
+                            }
+                        })
+                        .collect(),
+                    StructField::EmbeddedField(embedded_field) => {
+                        let field_name_serde_rename = &embedded_field.name();
+                        let field_name =
+                            format_ident!("{}", embedded_field.name().to_case(Case::Snake));
+                        let field_type = embedded_field.r#type();
+
+                        vec![quote! {
+                            #[serde(rename = #field_name_serde_rename)]
+                            pub #field_name: #field_type,
+                        }]
+                    }
+                })
+                .flatten()
+                .collect();
+
+            let token = quote! {
+                #[derive(::serde::Deserialize, Debug, Clone)]
+                pub struct #struct_name {
+                    #(#struct_fields)*
+                }
+            };
+
+            tokens.append_all(token);
+        }
     }
 }
--- golang-type-decl/golang-type-decl-macro/src/json_struct/mod.rs
+++ golang-type-decl/golang-type-decl-macro/src/json_struct/mod.rs
@@ -1,12 +1,10 @@
-use golang_type_decl_core::{golang_type_core::Type, TypeDecl, TypeSpec};
+use golang_type_decl_core::{golang_type_core::Type, type_def::JsonStructDef, TypeDecl, TypeSpec};
 use proc_macro2::TokenStream;
 use quote::quote;
 
 mod input;
-mod output;
 
 pub use self::input::Input;
-use self::output::Output;
 
 pub fn get_output(input: Input) -> TokenStream {
     let type_decl = match input.code.parse::<TypeDecl>() {
@@ -20,11 +18,11 @@ pub fn get_output(input: Input) -> TokenStream {
     let type_def = match type_decl.type_specs.first() {
         Some(TypeSpec::TypeDef(type_def)) => type_def,
         Some(TypeSpec::AliasDecl(_)) => {
-            let err = "";
+            let err = "Require [Type definitions](https://golang.org/ref/spec#TypeDef)";
             return quote!(compile_error!(#err));
         }
         None => {
-            let err = "";
+            let err = "Require [Type definitions](https://golang.org/ref/spec#TypeDef)";
             return quote!(compile_error!(#err));
         }
     };
@@ -33,15 +31,16 @@ pub fn get_output(input: Input) -> TokenStream {
     let struct_type = match &type_def.r#type {
         Type::StructType(struct_type) => struct_type,
         _ => {
-            let err = "";
+            let err =
+                "Require type definition [StructType](https://golang.org/ref/spec#StructType)";
             return quote!(compile_error!(#err));
         }
     };
 
-    let output = Output {
+    let json_struct_def = JsonStructDef {
         name: name.to_owned(),
         struct_type: struct_type.to_owned(),
     };
 
-    quote!(#output)
+    quote!(#json_struct_def)
 }
