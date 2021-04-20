90065e1979d6edac9eeff1c684d94ac15ed32066
vkill <vkill.net@gmail.com>
2021-04-20 11:15:33 +0800

    Refactor golang-type-decl

--- golang-type-decl/golang-type-decl-core/src/type_def.rs
+++ golang-type-decl/golang-type-decl-core/src/type_def.rs
@@ -53,41 +53,91 @@ pub mod enable_quote_to_tokens {
     use super::JsonStructDef;
 
     use convert_case::{Case, Casing as _};
-    use golang_type_core::StructField;
-    use proc_macro2::TokenStream;
+    use golang_type_core::{
+        golang_struct_tag::{JsonStructTag, JsonStructTagOption},
+        StructField, Type, TypeName,
+    };
+    use proc_macro2::{Punct, Spacing, TokenStream};
     use quote::{format_ident, quote, ToTokens, TokenStreamExt as _};
 
     impl ToTokens for JsonStructDef {
         fn to_tokens(&self, tokens: &mut TokenStream) {
-            let struct_name = format_ident!("{}", self.name.to_case(Case::Snake));
+            let struct_name = format_ident!("{}", self.name.to_case(Case::Pascal));
             let struct_fields: Vec<_> = self
                 .struct_type
                 .field_decls
                 .iter()
-                .map(|field_decl| match &field_decl.struct_field {
-                    StructField::IdentifierListType(names, r#type) => names
-                        .iter()
-                        .map(|name| {
-                            let field_name_serde_rename = name;
-                            let field_name = format_ident!("{}", name.to_case(Case::Snake));
-                            let field_type = r#type;
+                .map(|field_decl| {
+                    let as_json_struct_tag = if let Some(tag) = &field_decl.tag {
+                        tag.as_json_struct_tag()
+                    } else {
+                        None
+                    };
 
-                            quote! {
+                    let is_ignored = if let Some(JsonStructTag::Ignored) = as_json_struct_tag {
+                        Some(true)
+                    } else {
+                        None
+                    };
+                    if is_ignored == Some(true) {
+                        return vec![];
+                    }
+
+                    let rename = if let Some(JsonStructTag::Normal(rename, _)) = as_json_struct_tag
+                    {
+                        rename.to_owned()
+                    } else {
+                        None
+                    };
+                    let is_omitempty =
+                        if let Some(JsonStructTag::Normal(_, options)) = as_json_struct_tag {
+                            Some(options.contains(&JsonStructTagOption::Omitempty))
+                        } else {
+                            None
+                        };
+                    let is_string =
+                        if let Some(JsonStructTag::Normal(_, options)) = as_json_struct_tag {
+                            Some(options.contains(&JsonStructTagOption::String))
+                        } else {
+                            None
+                        };
+
+                    match &field_decl.struct_field {
+                        StructField::IdentifierListType(names, r#type) => names
+                            .iter()
+                            .filter(|x| x != &"_")
+                            .map(|name| {
+                                let field_name_serde_rename =
+                                    rename.to_owned().unwrap_or(name.to_owned());
+                                let field_name = format_ident!("r#{}", name.to_case(Case::Snake));
+                                let field_type = JsonStructFieldType {
+                                    r#type: *r#type.to_owned(),
+                                    is_string,
+                                    is_omitempty,
+                                };
+
+                                quote! {
+                                    #[serde(rename = #field_name_serde_rename)]
+                                    pub #field_name: #field_type,
+                                }
+                            })
+                            .collect(),
+                        StructField::EmbeddedField(embedded_field) => {
+                            let field_name_serde_rename =
+                                rename.to_owned().unwrap_or(embedded_field.name());
+                            let field_name =
+                                format_ident!("r#{}", embedded_field.name().to_case(Case::Snake));
+                            let field_type = JsonStructFieldType {
+                                r#type: embedded_field.r#type(),
+                                is_string,
+                                is_omitempty,
+                            };
+
+                            vec![quote! {
                                 #[serde(rename = #field_name_serde_rename)]
                                 pub #field_name: #field_type,
-                            }
-                        })
-                        .collect(),
-                    StructField::EmbeddedField(embedded_field) => {
-                        let field_name_serde_rename = &embedded_field.name();
-                        let field_name =
-                            format_ident!("{}", embedded_field.name().to_case(Case::Snake));
-                        let field_type = embedded_field.r#type();
-
-                        vec![quote! {
-                            #[serde(rename = #field_name_serde_rename)]
-                            pub #field_name: #field_type,
-                        }]
+                            }]
+                        }
                     }
                 })
                 .flatten()
@@ -99,6 +149,32 @@ pub mod enable_quote_to_tokens {
                     #(#struct_fields)*
                 }
             };
+
+            tokens.append_all(token);
+        }
+    }
+
+    struct JsonStructFieldType {
+        r#type: Type,
+        is_string: Option<bool>,
+        is_omitempty: Option<bool>,
+    }
+    impl ToTokens for JsonStructFieldType {
+        fn to_tokens(&self, tokens: &mut TokenStream) {
+            let r#type = &self.r#type;
+            let mut token = quote!(#r#type);
+            if self.is_string == Some(true) {
+                let r#type = Type::TypeName(TypeName::String);
+                token = quote!(#r#type);
+            }
+            if self.is_omitempty == Some(true) {
+                let mut tokens_tmp = TokenStream::new();
+                tokens_tmp.append_all(quote!(::core::option::Option));
+                tokens_tmp.append(Punct::new('<', Spacing::Alone));
+                tokens_tmp.append_all(token);
+                tokens_tmp.append(Punct::new('>', Spacing::Alone));
+                token = tokens_tmp;
+            }
 
             tokens.append_all(token);
         }
--- golang-type-decl/golang-type-decl/tests/gen_json_struct.rs
+++ golang-type-decl/golang-type-decl/tests/gen_json_struct.rs
@@ -1,15 +1,35 @@
+use std::error;
+
 use golang_type_decl::gen_json_struct;
 
 #[test]
-fn simple() {
+fn simple() -> Result<(), Box<dyn error::Error>> {
     gen_json_struct!(
         r#"
     type User struct {
         Name   string
         Age    uint   `json:",string"`
         Gender string `json:",omitempty"`
-        Addr   string `json:"address"`
+        Addr   string `json:"Address"`
     }
     "#
     );
+
+    let user: User = serde_json::from_str(
+        r#"
+{
+    "Name": "foo",
+    "Age": "20",
+    "Gender": null,
+    "Address": "bar"
+}
+"#,
+    )?;
+
+    assert_eq!(user.name, "foo");
+    assert_eq!(user.age, "20");
+    assert_eq!(user.gender, None);
+    assert_eq!(user.addr, "bar");
+
+    Ok(())
 }
