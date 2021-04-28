8951acff8d500ad6f63661b9501acb4cba9ede36
vkill <vkill.net@gmail.com>
2021-04-28 21:50:04 +0800

    Refactor golang-type-decl macro

--- golang-type-decl/golang-type-decl/src/lib.rs
+++ golang-type-decl/golang-type-decl/src/lib.rs
@@ -6,14 +6,34 @@ macro_rules! gen_json_struct {
     ($code:literal) => {
         golang_type_decl_macro::gen_json_struct!(code = $code,);
     };
-    ($code:literal, $( $opt_k:ident = $opt_v:literal $(,)? ),+) => {
-        golang_type_decl_macro::gen_json_struct!(code = $code, $( $opt_k = $opt_v ,)*);
+    (
+        $code:literal,
+        $( $opt_k:ident = $opt_v:literal),+ $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            code = $code,
+            $( $opt_k = $opt_v ,)*
+        );
     };
-    ($code:literal; $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt $(,)? ),* }$(,)? ),*) => {
-        golang_type_decl_macro::gen_json_struct!(code = $code, field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,);
+    (
+        $code:literal;
+        $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt ),* $(,)? } ),* $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            code = $code,
+            field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,
+        );
     };
-    ($code:literal, $( $opt_k:ident = $opt_v:literal $(,)? ),+; $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt $(,)? ),* }$(,)? ),*) => {
-        golang_type_decl_macro::gen_json_struct!(code = $code, $( $opt_k = $opt_v ,)* field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,);
+    (
+        $code:literal,
+        $( $opt_k:ident = $opt_v:literal ),+ $(,)?;
+        $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt ),* $(,)? } ),* $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            code = $code,
+            $( $opt_k = $opt_v ,)*
+            field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,
+        );
     };
 }
 
@@ -22,13 +42,33 @@ macro_rules! gen_json_struct_from_file {
     ($path:literal) => {
         golang_type_decl_macro::gen_json_struct!(path = $path,);
     };
-    ($path:literal, $( $opt_k:ident = $opt_v:literal $(,)? ),+) => {
-        golang_type_decl_macro::gen_json_struct!(path = $path, $( $opt_k = $opt_v ,)*);
+    (
+        $path:literal,
+        $( $opt_k:ident = $opt_v:literal),+ $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            path = $path,
+            $( $opt_k = $opt_v ,)*
+        );
     };
-    ($path:literal; $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt $(,)? ),* }$(,)? ),*) => {
-        golang_type_decl_macro::gen_json_struct!(path = $path, field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,);
+    (
+        $path:literal;
+        $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt ),* $(,)? } ),* $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            path = $path,
+            field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,
+        );
     };
-    ($path:literal, $( $opt_k:ident = $opt_v:literal $(,)? ),+; $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt $(,)? ),* }$(,)? ),*) => {
-        golang_type_decl_macro::gen_json_struct!(path = $path, $( $opt_k = $opt_v ,)* field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,);
+    (
+        $path:literal,
+        $( $opt_k:ident = $opt_v:literal ),+ $(,)?;
+        $( $field:literal => { $( $field_opt_k:literal : $field_opt_v:tt ),* $(,)? } ),* $(,)?
+    ) => {
+        golang_type_decl_macro::gen_json_struct!(
+            path = $path,
+            $( $opt_k = $opt_v ,)*
+            field_opts = $( $field => $( $field_opt_k -> $field_opt_v ,)* ,)* ,
+        );
     };
 }
