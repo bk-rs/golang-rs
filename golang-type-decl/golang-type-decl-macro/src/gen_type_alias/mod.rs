389857ef8fdaf952a2fa7828533972e9b988d6cb
vkill <vkill.net@gmail.com>
2021-05-01 10:33:48 +0800

    Update gen_type_alias macro

--- golang-type-decl/golang-type-decl-macro/src/gen_type_alias/mod.rs
+++ golang-type-decl/golang-type-decl-macro/src/gen_type_alias/mod.rs
@@ -18,12 +18,9 @@ pub fn get_output(input: Input) -> TokenStream {
         }
     };
 
-    let alias_decl = match type_decl.type_specs.into_iter().nth(input.nth) {
-        Some(TypeSpec::TypeDef(_)) => {
-            let err = "Require [Alias declarations](https://golang.org/ref/spec#AliasDecl)";
-            return quote!(compile_error!(#err));
-        }
-        Some(TypeSpec::AliasDecl(alias_decl)) => alias_decl,
+    let (name, r#type) = match type_decl.type_specs.into_iter().nth(input.nth) {
+        Some(TypeSpec::TypeDef(type_def)) => (type_def.name, type_def.r#type),
+        Some(TypeSpec::AliasDecl(alias_decl)) => (alias_decl.name, alias_decl.r#type),
         None => {
             let err = "Require [Alias declarations](https://golang.org/ref/spec#AliasDecl)";
             return quote!(compile_error!(#err));
@@ -31,8 +28,8 @@ pub fn get_output(input: Input) -> TokenStream {
     };
 
     let type_alias = TypeAlias {
-        name: alias_decl.name,
-        r#type: alias_decl.r#type,
+        name,
+        r#type,
         opt: TypeAliasOption {
             alias_name: input.alias_name,
             special_type: input.r#type.map(|ty| quote!(#ty)),
